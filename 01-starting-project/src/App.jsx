c1421e581d8b9161bd470de8ae925df64d096323
FireXBG <55113543+FireXBG@users.noreply.github.com>
2024-03-13 03:21:51 +0200

    Added props

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data.js'
 
 const reactDesctiptions = ['Fundamentals', 'Crutial', 'Core']
 
@@ -19,12 +20,40 @@ function Header() {
   )
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt="..." />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  )
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id='core-concepts'>
         <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept
+              {...CORE_CONCEPTS[1]}
+            />
+             <CoreConcept
+              {...CORE_CONCEPTS[2]}
+            />
+            <CoreConcept
+              {...CORE_CONCEPTS[3]}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
